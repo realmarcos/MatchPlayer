1a1ba881462ffd64adb07682e70d31a21f0a22ae
Marcos <realmarcos.raimundo@gmail.com>
2022-03-12 14:59:28 -0300

    🎨 Style: update style global

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -2,14 +2,14 @@ import { DefaultTheme } from 'react-native-paper'
 
 const theme = {
     ...DefaultTheme,
-    roundness: 50,
+    // roundness: 50,
     colors: {
         ...DefaultTheme.colors,
         primary: '#F28322',
         accent: '#f1c40f',
         background: '#FFFFFF',
         surface: '#3D1459',
-        // text: 'blue',
+        text: 'white',
         disabled: "#707070",
         placeholder: '#707070',
         backdrop: "#F26A4B",
