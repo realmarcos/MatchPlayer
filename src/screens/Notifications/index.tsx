5a008b6e8dd4e5c4e1f0d4659ea33bf4ad1f606a
Marcos <realmarcos.raimundo@gmail.com>
2022-03-17 21:39:52 -0300

    💄 UI: add style in screen Notifications

--- src/screens/Notifications/index.tsx
+++ src/screens/Notifications/index.tsx
@@ -1,14 +1,76 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { Avatar, Card, IconButton, Text, useTheme } from "react-native-paper";
+import { ButtonCards, ButtonPrimary } from "../../components/Buttons";
+import Header from "../../components/Header";
 import { styles } from "../../theme/styles";
 
 /**Screen Notifications */
 const Notifications = ({ navigation }: any) => {
-
+  const { colors } = useTheme();
   return (
-    <View style={styles.container}>
-      <Text>Notifications</Text>
-    </View>
+    <>
+      <Header title="Notificações">
+        <IconButton
+          icon="keyboard-backspace"
+          size={32}
+          color={colors.primary}
+          style={{ marginRight: -20 }}
+          onPress={() => navigation.goBack()}
+        />
+      </Header>
+      <ScrollView>
+        <View style={styles.container}>
+          <ButtonPrimary onPress={() => console.log('marcar como lido')}>marcar todas como lida</ButtonPrimary>
+          <Card style={styles.cards}>
+            <Card.Content>
+              <Text>
+                <Avatar.Icon size={14} icon="checkbox-marked-circle-outline" />
+                {" "}Fulano te enviou um convite para partida tall
+              </Text>
+            </Card.Content>
+            <Card.Actions style={styles.cardsActions2}>
+              <ButtonCards>aceitar</ButtonCards>
+              <ButtonCards>recusar</ButtonCards>
+            </Card.Actions>
+          </Card>
+          <Card style={styles.cards}>
+            <Card.Content>
+              <Text>
+                Fulano te enviou uma solicitação de amizade
+              </Text>
+            </Card.Content>
+            <Card.Actions style={styles.cardsActions2}>
+              <ButtonCards>aceitar</ButtonCards>
+              <ButtonCards>recusar</ButtonCards>
+            </Card.Actions>
+          </Card>
+          <Card style={styles.cards}>
+            <Card.Content>
+              <Text>
+                <Avatar.Icon size={14} icon="checkbox-marked-circle-outline" />
+                {" "}Fulano te enviou uma solicitação de amizade
+              </Text>
+            </Card.Content>
+            <Card.Actions style={styles.cardsActions}>
+              <ButtonCards>você aceitou</ButtonCards>
+            </Card.Actions>
+          </Card>
+          <Card style={styles.cards}>
+            <Card.Content>
+              <Text>
+                <Avatar.Icon size={14} icon="checkbox-marked-circle-outline" />
+                {" "}Fulano te enviou uma solicitação de amizade
+              </Text>
+            </Card.Content>
+            <Card.Actions style={styles.cardsActions}>
+              <ButtonCards>você recusou</ButtonCards>
+            </Card.Actions>
+          </Card>
+
+        </View>
+      </ScrollView>
+    </>
   );
 }
 export default Notifications;
