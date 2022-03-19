a7870e4eefaa8eae81ceff6a84e34fc22070a1fb
Marcos <realmarcos.raimundo@gmail.com>
2022-03-19 11:22:17 -0300

    💄 UI: add style in screens editProfile, Friends, and Profile

--- src/screens/Friends/index.tsx
+++ src/screens/Friends/index.tsx
@@ -1,22 +1,72 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Avatar, Card, IconButton, Searchbar, Text, Title, useTheme } from "react-native-paper";
+import { ButtonCards } from "../../components/Buttons";
+import Header from "../../components/Header";
+import { styles } from "../../theme/styles";
 
 /**Screen Friends */
 const Friends = ({ navigation }: any) => {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 25,
-      backgroundColor: "#FFFFFF",
-      // alignItems: 'center',  
-      justifyContent: 'center',
-    }
+  const { colors } = useTheme();
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = (query: any) => setSearchQuery(query);
+  const stylesLocal = StyleSheet.create({
   })
 
+  const listCards = () => {
+    const cards = [];
+    for (let i = 0; i < 10; i++) {
+      cards.push(
+        <Card style={styles.cards} key={i}>
+          <Card.Content>
+            <View style={styles.cardContentAvatar}>
+              <Avatar.Image size={64} source={require('../../assets/avatar.png')} />
+              <View style={{ margin: 5, marginLeft: 10 }}>
+                <Title>
+                  João da Silva
+                </Title>
+                <Text>Esporte favorito: Futebol </Text>
+              </View>
+            </View>
+          </Card.Content>
+
+          <Card.Actions style={styles.cardsActions}>
+            <ButtonCards>desfazer amizade</ButtonCards>
+          </Card.Actions>
+        </Card>
+      )
+    }
+    return cards;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Amigos</Text>
-    </View>
+    <>
+      <Header title="Amigos">
+        <IconButton
+          icon="keyboard-backspace"
+          size={32}
+          color={colors.primary}
+          style={{ marginRight: -20 }}
+          onPress={() => navigation.goBack()}
+        />
+      </Header>
+      <ScrollView>
+        <View style={styles.container}>
+          <View>
+            <Searchbar
+              autoComplete={"off"}
+              placeholder="Pesquisar"
+              onChangeText={onChangeSearch}
+              value={searchQuery}
+              theme={{ colors: { text: colors.placeholder } }}
+              style={styles.search}
+            />
+          </View>
+          {listCards()}
+        </View>
+
+      </ScrollView>
+    </>
   );
 }
 export default Friends;
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,22 +1,81 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Avatar, IconButton, Text, useTheme } from "react-native-paper";
+import { ButtonPrimary, ButtonSecondary } from "../../components/Buttons";
+import Header from "../../components/Header";
+import { styles } from "../../theme/styles";
 
 /**Screen Profile */
 const Profile = ({ navigation }: any) => {
-  const styles = StyleSheet.create({
-    container: {
+  const { colors } = useTheme();
+
+  const stylesLocal = StyleSheet.create({
+    avatarImg: {
+      alignItems: "center",
       flex: 1,
-      padding: 25,
-      backgroundColor: "#FFFFFF",
-      // alignItems: 'center',  
-      justifyContent: 'center',
+      marginBottom: 20
+    },
+    btns: {
+      marginTop: 20
     }
-  })
+  });
 
   return (
-    <View style={styles.container}>
-      <Text>Perfil</Text>
-    </View>
+    <>
+      <Header title="Perfil">
+        <IconButton
+          icon="keyboard-backspace"
+          size={32}
+          color={colors.primary}
+          style={{ marginRight: -20 }}
+          onPress={() => navigation.goBack()}
+        />
+      </Header>
+      <View style={styles.container}>
+        <View style={stylesLocal.avatarImg}>
+          <Avatar.Image style={{ borderWidth: 1, borderColor: colors.primary }} size={256} source={require('../../assets/avatar.png')} />
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text
+            style={{ fontSize: 18 }}
+            theme={{ colors: { text: colors.surface } }}
+          >
+            <Text style={{ fontWeight: "bold" }} theme={{ colors: { text: colors.surface } }}>Nome: </Text>João da Silva
+          </Text>
+          <Text
+            style={{ fontSize: 18 }}
+            theme={{ colors: { text: colors.surface } }}
+          >
+            <Text style={{ fontWeight: "bold" }} theme={{ colors: { text: colors.surface } }}>Usuário: </Text>@joao.silva
+          </Text>
+          <Text
+            style={{ fontSize: 18 }}
+            theme={{ colors: { text: colors.surface } }}
+          >
+            <Text style={{ fontWeight: "bold" }} theme={{ colors: { text: colors.surface } }}>Email: </Text>joao@gmail.com
+          </Text>
+          <Text
+            style={{ fontSize: 18 }}
+            theme={{ colors: { text: colors.surface } }}
+          >
+            <Text style={{ fontWeight: "bold" }} theme={{ colors: { text: colors.surface } }}>Número: </Text>(99) 9999-9999
+          </Text>
+          <Text
+            style={{ fontSize: 18 }}
+            theme={{ colors: { text: colors.surface } }}
+          >
+            <Text style={{ fontWeight: "bold" }} theme={{ colors: { text: colors.surface } }}>Esportes: </Text>Futebol, Vôlei
+          </Text>
+        </View>
+        <View style={stylesLocal.btns}>
+          <ButtonSecondary onPress={() => navigation.navigate('Friends')}>Amigos</ButtonSecondary>
+          <ButtonPrimary onPress={() => navigation.navigate('editProfile')}>editar perfil</ButtonPrimary>
+          <ButtonPrimary onPress={() => { console.log("alterar senha") }}>alterar senha</ButtonPrimary>
+
+        </View>
+
+      </View>
+    </>
   );
 }
 export default Profile;
